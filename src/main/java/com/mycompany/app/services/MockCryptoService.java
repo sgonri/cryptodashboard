5b843ee6a28197b0cb4152c7250a10f4736075a5
Valtteri Rinne <valtteri.rinne@tuni.fi>
2025-10-28 20:53:56 +0200

    prototype: add more mock data

--- src/main/java/com/mycompany/app/services/MockCryptoService.java
+++ src/main/java/com/mycompany/app/services/MockCryptoService.java
@@ -9,6 +9,7 @@ public class MockCryptoService implements CryptoService {
     @Override
     public List<Crypto> getTopCryptos() {
         List<Crypto> list = new ArrayList<>();
+        list.add(new Crypto("xrp", "XRP", "XRP", 6.00, 100, "$33.0B", "$800M", "50.0B"));
         list.add(new Crypto("btc", "Bitcoin", "BTC", 63421.00, 3.5, "$1.2T", "$32B", "19.0M"));
         list.add(new Crypto("eth", "Ethereum", "ETH", 3456.78, 2.37, "$415.6B", "$20B", "118.0M"));
         list.add(new Crypto("sol", "Solana", "SOL", 178.90, 3.58, "$35.8B", "$2.5B", "300.0M"));
